0c24d8f8cfea4d93d3a391181166183ff88297bc
Olexii Khyzhniak <63338851+OlexiiKhyzhniak@users.noreply.github.com>
2022-02-05 18:45:31 +0200

    Updates

---
+++ precache-manifest.3c29518cd220c4079efcf483dcd7b15a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f219ff2a04fb098d06de6656c9e9e7e2",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "78be287a89010342b307",
+    "url": "/todo-react/static/css/main.ffac1514.chunk.css"
+  },
+  {
+    "revision": "dca5cf80b922063a8eb5",
+    "url": "/todo-react/static/js/2.caa8e877.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/todo-react/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "78be287a89010342b307",
+    "url": "/todo-react/static/js/main.87933585.chunk.js"
+  },
+  {
+    "revision": "3fb3eec46481015aa288",
+    "url": "/todo-react/static/js/runtime~main.6986de36.js"
+  }
+]);
